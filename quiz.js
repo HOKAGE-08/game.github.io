e039947112a01023d16e4d00f2a6874ee0a329a6
Amit Rajesh Singh <65843278+HOKAGE-08@users.noreply.github.com>
2021-04-09 20:32:37 +0530

    college quiz game

---
+++ quiz.js
@@ -0,0 +1,136 @@
+var quiz = {
+  // (A) PROPERTIES 
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+  {
+    q : "An ________ is a set of entities of the same type that share the same properties, or attributes.",
+    o : [
+      "attribute set",
+      "Entity set",
+      "Relation set",
+      "Entity model"
+    ],
+    a : 1 // arrays start with 0, so answer is Entity set
+  },
+  {
+    q : "Entity is a _________",
+    o : [
+      "Object of relation",
+      "Present working model",
+      "Model of relation",
+      "Thing in real world"
+    ],
+    a : 3
+  },
+  {
+    q : "The descriptive property possessed by each entity set is _________",
+    o : [
+      "Entity",
+      "Relation",
+      "Attribute",
+      " Model"
+    ],
+    a : 2
+  },
+  {
+    q : "The function that an entity plays in a relationship is called that entity’s _____________",
+    o : [
+      "Role",
+      "Participation",
+      "Position",
+      "Instance"
+    ],
+    a : 0
+  },
+  {
+    q : "Not applicable condition can be represented in relation entry as",
+    o : [
+      "NA",
+      "0",
+      "Blank Space",
+      "NULL"
+    ],
+    a : 3
+  }
+  ],
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: function(){
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: function(){
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", quiz.select);
+      quiz.hAns.appendChild(label);
+    }
+  },
+  
+  // (D) OPTION SELECTED
+  select: function(){
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = this.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) { 
+      quiz.score++; 
+      this.classList.add("correct");
+    } else {
+      this.classList.add("wrong");
+    }
+  
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(function(){
+      if (quiz.now < quiz.data.length) { quiz.draw(); } 
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  }
+};
+window.addEventListener("load", quiz.init);
